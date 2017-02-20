4711fc2237300897c6c61969f826db0131f8c3a2
Aleksi Kervinen <me@aleksi.me>
2017-02-20 13:27:18 +0200

    change from i64 to f64

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-pub fn evaluate(expr: &str) -> Result<i64, String> {
-    Ok(0)
+pub fn evaluate(expr: &str) -> Result<f64, String> {
+    Ok(0.0)
 }
 
 #[cfg(test)]
@@ -8,21 +8,21 @@ mod tests {
 
     #[test]
     fn eval_one_positive_operand() {
-        assert_eq!(evaluate("1").unwrap(), 1);
-        assert_eq!(evaluate("543").unwrap(), 543);
+        assert_eq!(evaluate("1").unwrap(), 1.0);
+        assert_eq!(evaluate("543").unwrap(), 543.0);
     }
 
     #[test]
     fn eval_one_negative_operand() {
-        assert_eq!(evaluate("-1").unwrap(), -1);
-        assert_eq!(evaluate("-543").unwrap(), -543);
+        assert_eq!(evaluate("-1").unwrap(), -1.0);
+        assert_eq!(evaluate("-543").unwrap(), -543.0);
     }
 
-    /*#[test]
-    fn eval_one_decimal_operand() {
+    #[test]
+        fn eval_one_decimal_operand() {
         assert_eq!(evaluate("0.5").unwrap(), 0.5);
         assert_eq!(evaluate("-0.5").unwrap(), -0.5);
-    }*/
+    }
 
     #[test]
     fn eval_err_too_many_operands() {
@@ -37,46 +37,46 @@ mod tests {
 
     #[test]
     fn eval_add() {
-        assert_eq!(evaluate("1 2 +").unwrap(), 3);
+        assert_eq!(evaluate("1 2 +").unwrap(), 3.0);
     }
 
     #[test]
     fn eval_add_multiple() {
-        assert_eq!(evaluate("1 2 + 3 +").unwrap(), 6);
+        assert_eq!(evaluate("1 2 + 3 +").unwrap(), 6.0);
     }
 
     #[test]
     fn eval_subtract() {
-        assert_eq!(evaluate("1 2 -").unwrap(), -1);
+        assert_eq!(evaluate("1 2 -").unwrap(), -1.0);
     }
 
     #[test]
     fn eval_subtract_multiple() {
-        assert_eq!(evaluate("1 2 - 3 -").unwrap(), -4);
+        assert_eq!(evaluate("1 2 - 3 -").unwrap(), -4.0);
     }
 
     #[test]
     fn eval_multiply() {
-        assert_eq!(evaluate("1 2 *").unwrap(), 2);
+        assert_eq!(evaluate("1 2 *").unwrap(), 2.0);
     }
 
     #[test]
     fn eval_multiply_multiple() {
-        assert_eq!(evaluate("1 2 * 3 *").unwrap(), 6);
-    }
-    
-    /*#[test]
-    fn eval_divide() {
-        assert_eq!(evaluate("1 2 /").unwrap(), 2);
+        assert_eq!(evaluate("1 2 * 3 *").unwrap(), 6.0);
     }
 
     #[test]
-    fn eval_divide_multiple() {
-        assert_eq!(evaluate("1 2 * 3 *").unwrap(), 6);
-    }*/
+        fn eval_divide() {
+        assert_eq!(evaluate("1 2 /").unwrap(), 2.0);
+    }
+    
+    #[test]
+        fn eval_divide_multiple() {
+        assert_eq!(evaluate("1 2 * 3 *").unwrap(), 6.0);
+    }
 
     #[test]
     fn eval_mixed_operators() {
-        assert_eq!(evaluate("1 2 + 3 * 4 -").unwrap(), 5);
+        assert_eq!(evaluate("1 2 + 3 * 4 -").unwrap(), 5.0);
     }
 }
