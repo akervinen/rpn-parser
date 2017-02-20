b8ecaa7c8d95be8980f93deb102d41c57319e3a6
Aleksi Kervinen <me@aleksi.me>
2017-02-20 19:16:59 +0200

    use eval in main instead of raw tokens

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,5 @@
 extern crate rpn_parser;
 
 fn main() {
-    use rpn_parser::Token;
-    use rpn_parser::Token::*;
-
-    let tokens: Vec<Token> = vec![
-        Number(5.0),
-        Number(1.0),
-        Number(2.0),
-        Identifier("+".into()),
-        Number(4.0),
-        Identifier("x".into()),
-        Identifier("+".into()),
-        Number(3.0),
-        Identifier("-".into())
-    ];
-
-    println!("= {:?}", rpn_parser::execute(tokens).unwrap());
-
-    let tokens: Vec<Token> = vec![
-        Identifier("pi".into()),
-        Number(2.0),
-        Identifier("/".into()),
-        Identifier("sin".into())
-    ];
-
-    println!("= {:?}", rpn_parser::execute(tokens).unwrap());
+    println!("{:?}", rpn_parser::evaluate("5 1 2 + 4 × + 3 −").unwrap());
 }
