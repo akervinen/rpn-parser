e65eeb9e385ec82fc95e91737fcaf4b5f2d17e01
Aleksi Kervinen <me@aleksi.me>
2017-02-20 13:05:42 +0200

    add basic i64 evaluate function and tests for it

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,82 @@
+pub fn evaluate(expr: &str) -> Result<i64, String> {
+    Ok(0)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn eval_one_positive_operand() {
+        assert_eq!(evaluate("1").unwrap(), 1);
+        assert_eq!(evaluate("543").unwrap(), 543);
+    }
+
+    #[test]
+    fn eval_one_negative_operand() {
+        assert_eq!(evaluate("-1").unwrap(), -1);
+        assert_eq!(evaluate("-543").unwrap(), -543);
+    }
+
+    /*#[test]
+    fn eval_one_decimal_operand() {
+        assert_eq!(evaluate("0.5").unwrap(), 0.5);
+        assert_eq!(evaluate("-0.5").unwrap(), -0.5);
+    }*/
+
+    #[test]
+    fn eval_err_too_many_operands() {
+        assert!(evaluate("1 2").is_err());
+        assert!(evaluate("1 2 3 +").is_err());
+    }
+
+    #[test]
+    fn eval_err_not_enough_operands() {
+        assert!(evaluate("1 +").is_err());
+    }
+
+    #[test]
+    fn eval_add() {
+        assert_eq!(evaluate("1 2 +").unwrap(), 3);
+    }
+
+    #[test]
+    fn eval_add_multiple() {
+        assert_eq!(evaluate("1 2 + 3 +").unwrap(), 6);
+    }
+
+    #[test]
+    fn eval_subtract() {
+        assert_eq!(evaluate("1 2 -").unwrap(), -1);
+    }
+
+    #[test]
+    fn eval_subtract_multiple() {
+        assert_eq!(evaluate("1 2 - 3 -").unwrap(), -4);
+    }
+
+    #[test]
+    fn eval_multiply() {
+        assert_eq!(evaluate("1 2 *").unwrap(), 2);
+    }
+
+    #[test]
+    fn eval_multiply_multiple() {
+        assert_eq!(evaluate("1 2 * 3 *").unwrap(), 6);
+    }
+    
+    /*#[test]
+    fn eval_divide() {
+        assert_eq!(evaluate("1 2 /").unwrap(), 2);
+    }
+
+    #[test]
+    fn eval_divide_multiple() {
+        assert_eq!(evaluate("1 2 * 3 *").unwrap(), 6);
+    }*/
+
+    #[test]
+    fn eval_mixed_operators() {
+        assert_eq!(evaluate("1 2 + 3 * 4 -").unwrap(), 5);
     }
 }
